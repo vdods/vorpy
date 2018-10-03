d53c43ff2f18d62db73c9b47f0a19a1c437c2c11
Victor Dods <victor.dods@gmail.com>
2018-10-02 17:19:08 -0700

    Added scalar_cross_product_tensor to vorpy.linalg.

--- tests/test_linalg.py
+++ tests/test_linalg.py
@@ -2,6 +2,26 @@ import numpy as np
 import sympy as sp
 import vorpy.linalg
 
+def test_scalar_cross_product_tensor_symbolically ():
+    scalar_cross_product_tensor = vorpy.linalg.scalar_cross_product_tensor(dtype=sp.Integer)
+    u = np.array(sp.var('u_0,u_1'))
+    v = np.array(sp.var('v_0,v_1'))
+    # The awkward `[()]` notation on the left is to extract the scalar value from the 0-tensor result of np.cross
+    # when it is given 2d vector arguments.  The awkward, nested np.dot expression on the right is to avoid the lack
+    # of support for dtype=object in np.einsum.
+    assert np.all(np.cross(u, v)[()] == np.dot(np.dot(scalar_cross_product_tensor, v), u))
+    print('test_scalar_cross_product_tensor_symbolically passed.')
+
+def test_scalar_cross_product_tensor_float ():
+    scalar_cross_product_tensor = vorpy.linalg.scalar_cross_product_tensor(dtype=float)
+    rng = np.random.RandomState(666)
+    for i in range(100):
+        u = rng.randn(2)
+        for j in range(100):
+            v = rng.randn(2)
+            assert np.max(np.abs(np.cross(u,v)[()] - np.einsum('jk,j,k', scalar_cross_product_tensor, u, v))) < 1.0e-10
+    print('test_scalar_cross_product_tensor_float passed.')
+
 def test_cross_product_tensor_symbolically ():
     cross_product_tensor = vorpy.linalg.cross_product_tensor(dtype=sp.Integer)
     u = np.array(sp.var('u_0,u_1,u_2'))
@@ -21,5 +41,7 @@ def test_cross_product_tensor_float ():
     print('test_cross_product_tensor_float passed.')
 
 if __name__ == '__main__':
+    test_scalar_cross_product_tensor_symbolically()
+    test_scalar_cross_product_tensor_float()
     test_cross_product_tensor_symbolically()
     test_cross_product_tensor_float()
--- vorpy/linalg.py
+++ vorpy/linalg.py
@@ -1,14 +1,34 @@
 import numpy as np
 
+def scalar_cross_product_tensor (*, dtype=float):
+    """
+    Returns the 2-tensor which defines the scalar cross product in R^2, meaning that
+
+        np.cross(a, b)[()] == np.einsum('jk,j,k', scalar_cross_product_tensor(dtype=dtype), a, b)
+
+    where a and b have shape (2,) and dtype is compatible with the dtypes of a and b.  The
+    `[()]` syntax on the return value of np.cross is because it returns a numpy.ndarray having
+    shape () (i.e. a 0-tensor, aka a scalar), but the return value should be the scalar itself.
+
+    Note that because the cross product is defined as a tensor that is meant to contract with
+    indices, it can be applied to arguments having tensor order higher than 1 (i.e. the operands
+    don't have to be vectors).
+    """
+    retval = np.zeros((2,2), dtype=dtype)
+    retval[0,1] = dtype( 1)
+    retval[1,0] = dtype(-1)
+    return retval
+
 def cross_product_tensor (*, dtype=float):
     """
-    Returns the tensor which defines the cross product in R^3, meaning that
+    Returns the 3-tensor which defines the cross product in R^3, meaning that
 
         np.cross(a, b) == np.einsum('ijk,j,k->i', cross_product_tensor(dtype=dtype), a, b)
 
-    a and b have shape (3,) and dtype is compatible with the dtypes of a and b.  Note that
-    because the cross product is defined as a tensor that is meant to contract with indices,
-    it can be applied to arguments having tensor order higher than 1 (i.e. the operands
+    where a and b have shape (3,) and dtype is compatible with the dtypes of a and b.
+
+    Note that because the cross product is defined as a tensor that is meant to contract with
+    indices, it can be applied to arguments having tensor order higher than 1 (i.e. the operands
     don't have to be vectors).
     """
     retval = np.zeros((3,3,3), dtype=dtype)
